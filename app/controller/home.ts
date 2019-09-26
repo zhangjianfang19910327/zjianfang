7ddcecb34b5c4bef1c368fcd0197a2bb166e9160
zjf <2394758186@qq.com>
2019-09-26 17:08:55 +0800

    add

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -39,9 +39,9 @@ export default class HomeController extends Controller {
                   resolve(data);
             });
           });
-          console.log(kafka);
+         
         if(kafka){
-          ctx.body = {status: true, kafka: kafka};
+          ctx.body = {status: true, kafka: kafka,msg:"请登录邮箱验证注册！"};
         }
       }else{
         ctx.body = {status: false,spm: spm};
@@ -49,7 +49,7 @@ export default class HomeController extends Controller {
       console.log(spm,result);
       console.log(body);
     } catch (error) {
-      console.log(error);
+      ctx.body={status:false,msg:"服务器内部错误！"}
     }
   }
   public async html() {
@@ -82,6 +82,7 @@ export default class HomeController extends Controller {
       userinfo.description="";
       userinfo.password=userinfodata.password;
       const status = await ctx.connection.manager.save(userinfo);
+      console.log(status)
       ctx.body = status;
     } catch (error) {
       ctx.body =await {flag:false};
@@ -104,5 +105,124 @@ export default class HomeController extends Controller {
       ctx.body=await {status:false};
     }
   }
+  async qq() {
+    const query = this.ctx.query;
+    const client_id=101579655;
+    const state=query.state;
+    const grant_type='authorization_code';
+    const code=query.code;
+    const redirect_uri='https://www.zjianfang.com/api/qq';
+    const open_id_url='https://graph.qq.com/oauth2.0/me?access_token=';
+    const appkey='409257504ca4c23275b516b5dce2d315';
+    const token_url='https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=101579655&client_secret=409257504ca4c23275b516b5dce2d315&code='+code+'&redirect_uri='+redirect_uri;
+    const res=await this.ctx.curl(token_url,{
+      
+      timeout: 3000
+    });
+    // get请求字符串 转json
+    function parsePara(e) {
+      var t, n, r, i = e, s = {};
+      t = i.split("&"),
+      r = null,
+      n = null;
+      for (var o in t) {
+          var u = t[o].indexOf("=");
+          u !== -1 && (r = t[o].substr(0, u),
+          n = t[o].substr(u + 1),
+          s[r] = n)
+      }
+      return s
+  }
+    //buffer转换成字符串
+    let data=res.data.toString('utf8');
+    const datajson:any = parsePara(data); 
+    console.log("datajson:"+datajson);
+    const access_token =datajson.access_token;
+    const expires_in=datajson.expires_in;
+    const refresh_token=datajson.refresh_token;
+    const res_access_token=await this.ctx.curl(open_id_url+access_token,{
+      timeout: 3000
+    });
+    const open_id_json=JSON.parse((res_access_token.data.toString('utf8')).replace(/(callback|\(|\)|\;)/g,""));
+    console.log("open_id_json:"+open_id_json)
+    const openid=open_id_json.openid;
+    let userinfo=await this.ctx.curl('https://graph.qq.com/user/get_user_info?access_token='+access_token+'&oauth_consumer_key='+client_id+'&openid='+openid,{
+      timeout: 3000
+    });
+    userinfo=JSON.parse(userinfo.data.toString('utf8'));
+    console.log(userinfo);
+    this.ctx.body={login:true,userinfo:userinfo};
+  }
 
 }
+// query:
+// { code: 'B93042902BC54A2D17140E7A69168C51', state: 'state' }
+// res:
+// { data:
+//    <Buffer 61 63 63 65 73 73 5f 74 6f 6b 65 6e 3d 36 30 36 30 35 37 33 43 31 46 38 34 31 31 36 36 44 46 41 36 39 36 41 42 45 34 36 43 36 43 41 33 26 65 78 70 69 ... >,
+//   status: 200,
+//   headers:
+//    { date: 'Thu, 26 Sep 2019 03:59:57 GMT',
+//      'content-type': 'text/html',
+//      'content-length': '111',
+//      connection: 'keep-alive',
+//      server: 'QZHTTP-2.38.20',
+//      'cache-control': 'no-cache' },
+//   res:
+//    { status: 200,
+//      statusCode: 200,
+//      statusMessage: 'OK',
+//      headers:
+//       { date: 'Thu, 26 Sep 2019 03:59:57 GMT',
+//         'content-type': 'text/html',
+//         'content-length': '111',
+//         connection: 'keep-alive',
+//         server: 'QZHTTP-2.38.20',
+//         'cache-control': 'no-cache' },
+//      size: 111,
+//      aborted: false,
+//      rt: 212,
+//      keepAliveSocket: false,
+//      data:
+//       <Buffer 61 63 63 65 73 73 5f 74 6f 6b 65 6e 3d 36 30 36 30 35 37 33 43 31 46 38 34 31 31 36 36 44 46 41 36 39 36 41 42 45 34 36 43 36 43 41 33 26 65 78 70 69 ... >,
+//      requestUrls:
+//       [ 'https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=101579655&client_secret=409257504ca4c23275b516b5dce2d315&code=B93042902BC54A2D17140E7A69168C51&redirect_uri=https://www.zjianfang.com/api/qq' ],
+//      timing: null,
+//      remoteAddress: '113.96.208.232',
+//      remotePort: 443,
+//      socketHandledRequests: 1,
+//      socketHandledResponses: 1 } }
+// datajson:
+// { access_token: '6060573C1F841166DFA696ABE46C6CA3',
+//   expires_in: '7776000',
+//   refresh_token: '6DA5E2CD6579D9F26CD9B5D608D62F0F' }
+// open_id_json:
+// { client_id: '101579655',
+//   openid: 'CBFBEAEB98A0864762ABE244BC0F17BC' }
+// { ret: 0,
+//   msg: '',
+//   is_lost: 0,
+//   nickname: '买买买提',
+//   gender: '男',
+//   province: '河南',
+//   city: '郑州',
+//   year: '1991',
+//   constellation: '',
+//   figureurl:
+//    'http://qzapp.qlogo.cn/qzapp/101579655/CBFBEAEB98A0864762ABE244BC0F17BC/30',
+//   figureurl_1:
+//    'http://qzapp.qlogo.cn/qzapp/101579655/CBFBEAEB98A0864762ABE244BC0F17BC/50',
+//   figureurl_2:
+//    'http://qzapp.qlogo.cn/qzapp/101579655/CBFBEAEB98A0864762ABE244BC0F17BC/100',
+//   figureurl_qq_1:
+//    'http://thirdqq.qlogo.cn/g?b=oidb&k=ZJe3I1oyPuXOxicWVP4tdBw&s=40&t=1558580030',
+//   figureurl_qq_2:
+//    'http://thirdqq.qlogo.cn/g?b=oidb&k=ZJe3I1oyPuXOxicWVP4tdBw&s=100&t=1558580030',
+//   figureurl_qq:
+//    'http://thirdqq.qlogo.cn/g?b=oidb&k=ZJe3I1oyPuXOxicWVP4tdBw&s=140&t=1558580030',
+//   figureurl_type: '1',
+//   is_yellow_vip: '0',
+//   vip: '0',
+//   yellow_vip_level: '0',
+//   level: '0',
+//   is_yellow_year_vip: '0' }
